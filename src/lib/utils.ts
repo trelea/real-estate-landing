f979730356f978d3323b794eb93c5777e1302877
trelea dev <treleamarius76@gmail.com>
2025-09-15 01:58:41 +0300

    filtering pre-middleware action

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const checkFilters = (queryUrl: unknown) =>
+  Object.entries(queryUrl as Record<string, any>)
+    .filter(([key, _]) => {
+      if (key === "page") return false;
+      if (key === "sort") return false;
+      return true;
+    })
+    .filter(([_, value]) => {
+      if (!value) return false;
+      if (value === null) return false;
+      if (Array.isArray(value) && value.length === 0) return false;
+      return true;
+    }).length !== 0;
